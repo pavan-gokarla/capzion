f456109268fd0d95179d6ba27d95d105ff02d391
pavangokarlag@gmail.com <pavangokarlag@gmail.com>
2025-04-03 21:06:04 +0530

    changed page.tsx to upload-audio.tsx

--- components/ui/upload-audio.tsx
+++ components/ui/upload-audio.tsx
@@ -1,16 +1,30 @@
 
-import React, { useState } from 'react'
+import React, { use, useEffect, useState } from 'react'
 import ClipLoader from "react-spinners/ClipLoader";
 import { uploadAudio } from "@/actions/uploadAudio"
 
 import { FileUpload } from './file-upload'
 import { Button } from './button'
 import { toast } from 'sonner';
+import { Link } from 'lucide-react';
 
 const FileManage = () => {
     const [file, setFile] = useState<File | null>(null)
     const [transcriptFile, setTranscriptFile] = useState<string | null>(null)
     const [loading, setLoading] = useState(false)
+    const [downloadUrl, setDownloadUrl] = useState<string | null>(null)
+    useEffect(() => {
+        if (transcriptFile) {
+            const url = URL.createObjectURL(new Blob([transcriptFile], { type: 'text/plain' }))
+            setDownloadUrl(url)
+        }
+        return () => {
+            if (downloadUrl) {
+                URL.revokeObjectURL(downloadUrl)
+            }
+        }
+    }, [transcriptFile])
+
     return (
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-20" id="upload-audio">
             {
@@ -47,12 +61,14 @@ const FileManage = () => {
                 </Button>
             </div>
             {
-                transcriptFile && (
-                    <div className="bg-slate-800/50 p-6 rounded-xl border border-slate-700 mt-8 flex justify-around ">
-                        <p className="text-slate-400">Transcript file: {transcriptFile}</p>
-                        <Button onClick={async () => {
+                downloadUrl && (
+                    <div className="bg-slate-800/50 p-3 rounded-xl border border-slate-700 mt-8 flex justify-around ">
+                        <p className="text-slate-400 mt-1.5 ">Transcript file ready</p>
+                        <a href={downloadUrl} onClick={async () => {
                             setTranscriptFile(null)
-                        }} className='bg-slate-300/50 cursor-pointer ' >Remove</Button>
+                            setDownloadUrl(null)
+                        }} download="transcript.txt"  ><Button className='bg-slate-300/50 cursor-pointer' >Download</Button></a>
+
                     </div>
                 )
             }
